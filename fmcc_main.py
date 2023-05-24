154b33388dc5575f3842a6d00491f78b095be596
2on9 <ui2on9h@gmail.com>
2023-05-24 15:09:34 +0900

    main에 함수로 바꿔놓음
    삭제하고 main에 함수로 넣어놨어용

--- fmcc_main.py
+++ fmcc_main.py
@@ -1,7 +1,8 @@
-#import os
+import os
 import numpy as np
 import librosa as lr
 import soundfile as sf
+import fmcc_denoiser
 #from rpy2.robjects import pandas2ri, packages as robjects
 #pandas2ri.activate()
 #stats = packages.importr('stats')
@@ -33,8 +34,8 @@ def readTrainWav():
         wav_data = lr.util.buf_to_float(x=pcm_data, n_bytes=2)
         sf.write(destinationPath, wav_data, 16000, format='WAV', endian='LITTLE', subtype='PCM_16')
         print(destinationPath+" done...")
-    
-    '''
+        
+        '''
     for target in trainNames:
         if(target[0]=="F"):
             dest="R/female/"
@@ -50,6 +51,31 @@ def readTrainWav():
         sf.write(destinationPath, wav_data, 16000, format='WAV', endian='LITTLE', subtype='PCM_16')
         print(destinationPath+" done...")
         '''
+        
+# 학습용 wav 잡음 삭제 -> R_train 저장
+def denoiseTrainWav():
+        
+    train_path = 'raw16k/train_wav'
+    train_file_list = os.listdir(train_path)
+
+    for sex in train_file_list:
+        if sex == 'female':
+            train_wav_path = 'raw16k/train_wav/female'
+            train_wav_denoise_path = 'R_train/female'
+            file_list = os.listdir(train_wav_path)
+            wav_files = [file for file in file_list if file.endswith('.wav')]
+    
+        elif sex == 'male':
+            train_wav_path = 'raw16k/train_wav/male'
+            train_wav_denoise_path = 'R_train/male'
+            file_list = os.listdir(train_wav_path)
+            wav_files = [file for file in file_list if file.endswith('.wav')]
+    
+        for wav_file in wav_files:
+            fileName = train_wav_path + "/" + wav_file
+            dest = train_wav_denoise_path + "/" + wav_file[0:13] + "_denoise.wav"
+            fmcc_denoiser.denoiseWav(fileName, dest)
+    
 
 #평가용 wav로 변환
 def readTestWav():
@@ -75,6 +101,30 @@ def readTestWav():
         sf.write(destinationPath, wav_data, 16000, format='WAV', endian='LITTLE', subtype='PCM_16')
         print(destinationPath+" done...")
 
+# 평가용 wav 잡음 삭제 -> R_test 저장
+def denoisTestWav():
+        
+    test_path = 'raw16k/test_wav'
+    test_file_list = os.listdir(test_path)
+
+    for sex in test_file_list:
+        if sex == 'female':
+            test_wav_path = 'raw16k/test_wav/female'
+            test_wav_denoise_path = 'R_test/female'
+            file_list = os.listdir(test_wav_path)
+            wav_files = [file for file in file_list if file.endswith('.wav')]
+    
+        elif sex == 'male':
+            test_wav_path = 'raw16k/test_wav/male'
+            test_wav_denoise_path = 'R_test/male'
+            file_list = os.listdir(test_wav_path)
+            wav_files = [file for file in file_list if file.endswith('.wav')]
+    
+        for wav_file in wav_files:
+            fileName = test_wav_path + "/" + wav_file
+            dest = test_wav_denoise_path + "/" + wav_file[0:13] + "_denoise.wav"
+            fmcc_denoiser.denoiseWav(fileName, dest)
+            
 #R 스크립트 불러오기
 '''
 def writeCSV():
