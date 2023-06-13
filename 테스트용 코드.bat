b34f12a3db0888e1a8d1d715a691b7a332ca4f72
Notpepsi <47716620+COkedat@users.noreply.github.com>
2023-06-13 18:45:34 +0900

    Update 테스트용 코드.bat

--- 테스트용 코드.bat
+++ 테스트용 코드.bat
@@ -3,7 +3,7 @@
 :_MENU
 cls
 echo (처음일 경우 1-2 순서대로 실행)
-echo 1. SVM 결정
+echo 1. SVM 결과예측
 echo 2. eval.pl 실행
 echo 0. 종료
 choice /c:120
@@ -15,9 +15,9 @@ echo 종료..
 pause
 exit 
 
-rem 1. SVM 결정
+rem 1. SVM 결과예측
 :_E1
-echo SVM 결정...
+echo SVM 결과예측...
 start /wait python fmcc_main.py predict
 pause
 goto _MENU
