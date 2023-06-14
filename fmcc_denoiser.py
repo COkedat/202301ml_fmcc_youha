47aaa21a59899d6017aa0aa46b96aa186f22f74c
2on9 <ui2on9h@gmail.com>
2023-06-14 15:50:01 +0900

    denoiser 변경
    denoiser master로 변경

--- fmcc_denoiser.py
+++ fmcc_denoiser.py
@@ -6,7 +6,7 @@ from denoiser.dsp import convert_audio
 
 #torch cpu로 변경 (cuda는 torch 버젼 문제나 그런거 생길 수 있어서 그냥 CPU로 통일)
 def denoiseWav(fileName, destName):
-    model = pretrained.dns64()
+    model = pretrained.master64()
     wav, sr = torchaudio.load(fileName)
     with torch.no_grad():
         denoised = model(wav[None])[0].cpu()
